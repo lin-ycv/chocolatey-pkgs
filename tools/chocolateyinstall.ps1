a941839e1e3b93c7c7f7566838b8d2fff43fe419
Victor Lin <victorlin@gapps.ntust.edu.tw>
2025-05-06 19:26:34 +0800

    Everything 1.5a

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$url = "https://www.voidtools.com/Everything-1.5.0.1391a.x86-Setup.exe"
+$url64 = "https://www.voidtools.com/Everything-1.5.0.1391a.x64-Setup.exe"
+
+Install-ChocolateyPackage -PackageName 'Everything Alpha' -FileType 'exe' -SilentArgs '/S' -Url $url -Url64bit $url64 
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName         = 'Everything Alpha'
+$softwareNamePattern = 'Everything *'
+
+
+[array] $key = Get-UninstallRegistryKey $softwareNamePattern
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        $key.UninstallString
+        $packageArgs = @{
+            packageName            = $packageName
+            silentArgs             = "-unsetup -silent"
+            fileType               = 'EXE'
+            validExitCodes         = @(0)
+            file                   = ''
+        }
+        $packageArgs.file = "$($_.UninstallString.Replace(' -unsetup', ''))"   #C:\Program Files\Everything 1.5a\Everything64.exe -unsetup
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {Write-Warning "- $_.DisplayName"}
+}
