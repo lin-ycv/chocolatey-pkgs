e0bd7c26bac468c89f2c2089dcab0b65b86f833b
lin-ycv <60565737+lin-ycv@users.noreply.github.com>
2025-05-28 09:32:25 +0000

    Updated - windows-app-runtime16  2025-05-28

--- manifests/windows-app-runtime16/tools/chocolateyinstall.ps1
+++ manifests/windows-app-runtime16/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $filename = "windowsappruntimeinstall16.exe"
 $args = @{
   packageName = $env:ChocolateyPackageName
   fileFullPath = "$env:TEMP\$filename"
-  url = 'https://aka.ms/windowsappsdk/1.6/1.6.250402001/windowsappruntimeinstall-x86.exe'
-  checksum = 'E9F5ACADACF400834395C760457FD2750376CC3931CEE805020608C84FF83488'
+  url = 'https://aka.ms/windowsappsdk/1.6/1.6.250430001/windowsappruntimeinstall-x86.exe'
+  checksum = 'ffe442fcf261c7dc98fefb9c690924259d0df0211f1403c6f451d04afd5a0169'
   checksumType = 'sha256'
-  url64Bit = 'https://aka.ms/windowsappsdk/1.6/1.6.250228001/windowsappruntimeinstall-x64.exe'
-  checksum64 = '94A9561AE2E679D0D63EBA522C152E88A4EBAC157843E2B9BBEDBD308F285AE0'
+  url64Bit = 'https://aka.ms/windowsappsdk/1.6/1.6.250430001/windowsappruntimeinstall-x64.exe'
+  checksum64 = '91afd354b53fb0215c71f22ab48441897cec3b315fd21a927bd2ce49ed1381e5'
   checksumType64= 'sha256'
 }
 Get-ChocolateyWebFile @args
