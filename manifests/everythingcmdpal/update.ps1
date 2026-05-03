85f54bac6b448d0e1c73a1260473673b9731d515
Lin Yu-Chieh (Victor) <victorlin@gapps.ntust.edu.tw>
2026-05-03 20:02:07 +0800

    remove ecp arm

--- manifests/everythingcmdpal/update.ps1
+++ manifests/everythingcmdpal/update.ps1
@@ -2,14 +2,8 @@ Import-Module Chocolatey-AU
 
 function global:au_GetLatest {
     $obj = Invoke-RestMethod -Uri "https://api.github.com/repos/lin-ycv/EverythingCommandPalette/releases/latest" -UseBasicParsing
-    $armhashpattern = "\s?msix-arm64\s?\|\s?([a-zA-Z0-9]{64})"
-    if (-not ($obj.body -match $armhashpattern)) {
-        Write-Output "No hash for arm found."
-        return
-    }
-    $armhash = $matches[1]
-    $x64hashpattern = "\s?msix-x64\s?\|\s?([a-zA-Z0-9]{64})"
-    if (-not ($obj.body -match $x64hashpattern)) {
+    $x64hashpattern = "sha256:([a-zA-Z0-9]{64})"
+    if (-not ($obj.assets.digest -match $x64hashpattern)) {
         Write-Output "No hash for x64 found."
         return
     }
@@ -19,10 +13,8 @@ function global:au_GetLatest {
     #returns into $Lastest
     @{
         Version = $version
-        ARM64 = $obj.assets.browser_download_url | Where-Object { $_ -match "$version.*arm64" }
         URL64 = $obj.assets.browser_download_url | Where-Object { $_ -match "$version.*x64" }
 	    Checksum64 = $x64hash
-	    Checksuma64 = $armhash
     }
 }
 
@@ -31,8 +23,6 @@ function global:au_SearchReplace{
         ".\tools\chocolateyinstall.ps1" = @{
             "(?i)(^\`$url\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(?i)(^\`$hash\s*=\s*)('[a-zA-Z0-9]{64}')" = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^[\s\t]+\`$url\s*=\s*)('.*')" = "`$1'$($Latest.ARM64)'"
-            "(?i)(^[\s\t]+\`$hash\s*=\s*)('[a-zA-Z0-9]{64}')" = "`$1'$($Latest.Checksuma64)'"
         }
     }
 }
